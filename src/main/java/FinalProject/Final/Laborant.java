17ef67cb7a26653b8a7cb53a7d7b829a1d0592d6
Tamir Dresher <tamir.dresher@gmail.com>
2022-02-28 08:51:50 +0200

    Update Laborant.java

--- src/main/java/FinalProject/Final/Laborant.java
+++ src/main/java/FinalProject/Final/Laborant.java
@@ -8,7 +8,7 @@ public class Laborant {
 		for(potentialPatient pat: poten){
 			if(pat.getPotentialPatientID().equals(PatientID)) {
 				if(isCovidPositive) {
-					patients.add(new PatientMedicalFile(new Patient(), new ArrayList<LabTestResult>()))
+					patients.add(new PatientMedicalFile(new Patient(), new ArrayList<LabTestResult>())) 
 					patients.get(patients.indexOf(pat)).getLabResults().add(new LabTestResult(labID,testID,PatientID,TestDate,isCovidPositive));
 					poten.remove(pat);
 					return patients.get(patients.indexOf(pat)).getLabResults().get(patients.get(patients.indexOf(pat)).getLabResults().size()-1).getPatientID();
